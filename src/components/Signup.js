cf543a386ef711e4fa95c6f668f929f78734ea2b
Shirene Boyd <shirenekadkhodai@gmail.com>
2022-04-01 09:12:01 -0500

    working app

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,18 +1,18 @@
 import React, { useRef } from "react";
 import { Form, Button, Card } from "react-bootstrap";
-import { useAuth } from "../contexts.js/AuthContext"
+// import { useAuth } from "../contexts.js/AuthContext"
 
 export default function Signup() {
 
 const emailRef = useRef()
 const passwordRef = useRef()
 const passwordConfirmRef = useRef()
-const { signup } = useAuth()
+// const { signup } = useAuth()
 
 function handleSubmit(e) {
     e.preventDefault()
     
-    signup(emailRef.current.value, passwordRef.current.value)
+    // signup(emailRef.current.value, passwordRef.current.value)
 }
 
   return (
